c4193618467b745edf79af4c940e7af937352ef7
MAXIRIVERO1 <maximilianoa.castillorivero@gmail.com>
2024-10-09 18:02:14 -0300

    last correction

--- src/routes/taskRouter.js
+++ src/routes/taskRouter.js
@@ -1,19 +1,19 @@
-const taskRouter = require("express").Router();
-
 const { createTaskController } = require("../controllers/taskController.js");
 const { getAllTaskController } = require("../controllers/taskController.js");
 const { getTaskByIdController } = require("../controllers/taskController.js");
 const { putTaskController } = require("../controllers/taskController.js");
 const { deleteTaskController } = require("../controllers/taskController.js");
- taskRouter
-.post("/create", createTaskController); taskRouter
-.get("/getAll", getAllTaskController); taskRouter
-.get("/:id", getTaskByIdController); taskRouter
-.put("/update/:id", putTaskController); taskRouter
-.delete("/delete/:id", deleteTaskController);
+
+const taskRouter = require("express").Router();
+
+taskRouter.post("/create", createTaskController); 
+taskRouter.get("/getAll", getAllTaskController); 
+taskRouter.get("/:id", getTaskByIdController); 
+taskRouter.put("/update/:id", putTaskController); 
+taskRouter.delete("/delete/:id", deleteTaskController);
 
 
 
 module.exports = {
- taskRouter
+    taskRouter
 };
